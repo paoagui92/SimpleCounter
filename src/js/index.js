2c4fda22d5f7b9fce3464ce31654159d5e4be703
Paola Aguirre <poagui92@gmail.com>
2020-09-13 22:34:28 -0400

    Contador simple

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
@@ -9,7 +10,42 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+export function SecondsCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="clock">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="fourthDig">{props.digitFour % 10}</div>
+			<div className="thirdDig">{props.digitThree % 10}</div>
+			<div className="secondDig">{props.digitTwo % 10}</div>
+			<div className="firstDig">{props.digitOne % 10}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SecondsCounter.propTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+
+let counter = 0;
+setInterval(() => {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	counter++;
+	//render your react application
+	ReactDOM.render(
+		<SecondsCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#root")
+	);
+}, 1000);
